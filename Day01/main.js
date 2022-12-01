dbdb3c96cf47f246f96be87858a4db1123f477a2
Spyon <tdpb@gmx.de>
2022-12-01 13:46:15 +0100

    day 01 ergänzungen

--- Day01/main.js
+++ Day01/main.js
@@ -1,1 +1,29 @@
 // --- Day 1: Calorie Counting ---
+
+// input
+var lines = readTextFile("input.txt");
+var numbers = lines.map(Number);
+
+
+// Part 1
+var increases = 0;
+for (var i = 1; i < numbers.length; i++) {    
+    if (numbers[i] > numbers[i - 1])    
+    {
+        increases++;
+    }    
+}
+console.log("Part 1:")
+console.log("Increases: %d", increases);
+
+
+// Part 2
+var increases = 0;
+for (var i = 1; i < numbers.length - 2; i++) {    
+    if (numbers[i] + numbers[i + 1] + numbers[i + 2] > numbers[i - 1] + numbers[i + 0] + numbers[i + 1])    
+    {
+        increases++;
+    }    
+}
+console.log("Part 2: windows measurement")
+console.log("Increases: %d", increases);
